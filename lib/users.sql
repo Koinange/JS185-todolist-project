9a362356fb3772c61e52cb21f61225d91638fdf5
Zack Muhia <koinangemuhia@gmail.com>
2023-03-13 19:29:58 +0000

    Initial commit

---
+++ lib/users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (username, password)
+  VALUES ('admin', '$2b$10$WpW3y7BJfY5XpeVY79HAt.DQLI.YET71853fEhL/y0yoe2eMZgvJC'),
+         ('developer', '$2b$10$gxckTAL9PFLJuM/O3uvuHu9C1oHoG1qPyln.RtJCg/ku7x7cLcKiq'),
+         ('somebody', 'knock-knock');
